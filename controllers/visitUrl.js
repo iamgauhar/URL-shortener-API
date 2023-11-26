689fd15de1ecbb9afd239c403c9740ede56d3e12
iamgauhar <iamgauhar@yahoo.com>
2023-11-26 17:16:07 +0530

    Bug fixe

--- controllers/visitUrl.js
+++ controllers/visitUrl.js
@@ -4,6 +4,8 @@ import connection from "../configs/db.js";
 export const visitUrl = async (req, res) => {
 
     const short_url = req.params.url;
+    console.log(short_url)
+
     try {
 
         const url = await connection.execute(`SELECT * FROM url_mapping WHERE short_url = "${short_url}"`)
@@ -11,13 +13,12 @@ export const visitUrl = async (req, res) => {
             status: false,
             message: "URL not found!"
         })
-
-        console.log(url[0][0].count_clicks)
+        console.log(url)
         let clicks = url[0][0].count_clicks + 1
 
         await connection.execute(`UPDATE url_mapping SET count_clicks = ${clicks} WHERE short_url = "${short_url}"`)
 
-        return res.redirect(url[0][0].original_url)
+        return res.status(200).json({ redirect_url: url[0][0].original_url, reponse: true })
 
     } catch (err) {
         return res.status(404).json({
